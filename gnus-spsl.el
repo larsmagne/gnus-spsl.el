d16010f15fcd6463207145292831c3a7058e5822
Lars Ingebrigtsen <larsi@gnus.org>
2026-06-05 12:08:34 +0200

    Scaffolding

---
+++ gnus-spsl.el
@@ -0,0 +1,25 @@
+;;; gnus-spsl.el --- Handle email spam splitting -*- lexical-binding: t -*-
+
+;; Copyright (C) 2026 Lars Ingebrigtsen.
+
+;; Author: Lars Magne Ingebrigtsen <larsi@gnus.org>
+
+;; gnus-spsl is free software; you can redistribute it and/or modify it
+;; under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 2, or (at your option)
+;; any later version.
+
+;; gnus-spsl is distributed in the hope that it will be useful, but WITHOUT
+;; ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+;; or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public
+;; License for more details.
+
+;;; Commentary:
+
+;;; Code:
+
+(require 'cl-lib)
+
+(provide 'gnus-spsl)
+
+;;; gnus-spsl.el ends here
